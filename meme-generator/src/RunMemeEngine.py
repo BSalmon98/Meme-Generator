3741db1afe1eada047854057b861c88adc62d2c5
BSalmon98 <91951688+BSalmon98@users.noreply.github.com>
2021-11-13 15:37:43 +0000

    Delete RunMemeEngine.py

--- meme-generator/src/RunMemeEngine.py
+++
@@ -1,3 +0,0 @@
-from MemeEngine import ImageHandler
-
-path = ImageHandler.process_image(r'D:\Billy\Documents\Coding\Python Intermediate Course\Project Meme Generator\meme-generator-starter-code\src\_data\photos\dog\xander_1.jpg', 'He who hath smelt it, doth dealt it', '-Fatty Bumble')
